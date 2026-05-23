baeec6ae3db605f524a28d0bfb59cae1c37a4f5c
taratalexander <46709650+taratalexander@users.noreply.github.com>
2026-05-24 01:34:48 +0200

    Implemented GPIO Interrupt related handlers

--- drivers/Inc/stm32f103xx.h
+++ drivers/Inc/stm32f103xx.h
@@ -13,6 +13,30 @@
  */
 #define __vo		volatile
 
+/*********************PROCESSOR SPECIFIC DETAILS***************************************************************************************/
+/*
+ * ARM Cortex M3 Processor NVIC ISERx register Addresses
+ */
+
+#define NVIC_ISER0				( (__vo uint32_t*) 0xE000E100 )
+#define NVIC_ISER1				( (__vo uint32_t*) 0xE000E104 )
+#define NVIC_ISER2				( (__vo uint32_t*) 0xE000E108 )
+#define NVIC_ISER3				( (__vo uint32_t*) 0xE000E10C )
+
+/*
+ * ARM Cortex M3 Processor NVIC ICERx register Addresses
+ */
+
+#define NVIC_ICER0				( (__vo uint32_t*) 0XE000E180 )
+#define NVIC_ICER1				( (__vo uint32_t*) 0XE000E184 )
+#define NVIC_ICER2				( (__vo uint32_t*) 0XE000E188 )
+#define NVIC_ICER3				( (__vo uint32_t*) 0XE000E18C )
+
+/*
+ * ARM Cortex M3 Processor Priority Register Address Calculations
+ */
+#define NVIC_PR_BASEADDR		( (__vo uint32_t*) 0xE000E400 )
+#define NO_PR_BITS_IMPLEMENTED 		4
 
 #define FLASH_BASEADDR			0x08000000U 	//Refer Reference Manual for base addresses
 #define SRAM1_BASEADDR			0x20000000U
@@ -58,7 +82,7 @@
 /*
  *  Peripherals hanging on APB2 bus
  */
-
+#define AFIO_BASEADDR				APB2PERIPH_BASEADDR
 #define EXT1_BASEADDR 				(APB2PERIPH_BASEADDR + 0x00000400U)
 #define SPI1_BASEADDR 				(APB2PERIPH_BASEADDR + 0x00003000U)
 #define USART1_BASEADDR 			(APB2PERIPH_BASEADDR + 0x00003800U)
@@ -71,6 +95,9 @@
 
 /**************************************peripheral register definition structures**************************************/
 
+/**
+ * Peripheral register definition structure of GPIO
+ */
 typedef struct
 {
 	__vo uint32_t CRL;				/*Port configuration register low , 			Address Offset:0x00*/
@@ -82,7 +109,9 @@ typedef struct
 	__vo uint32_t LCKR;				/*Port configuration lock register, 			Address Offset:0x18*/
 }GPIO_RegDef_t;
 
-
+/**
+ * Peripheral register definition structure of RCC
+ */
 typedef struct
 {
 	__vo uint32_t CR;					/*Clock control register, 						Address Offset:0x00*/
@@ -97,6 +126,33 @@ typedef struct
 	__vo uint32_t CSR;					/*Control/status register,			 			Address Offset:0x2C*/
 }RCC_RegDef_t;
 
+/**
+ * Peripheral register definition structure of EXTI
+ */
+typedef struct
+{
+	__vo uint32_t IMR;				/*Interrupt mask register,	 					Address Offset:0x00*/
+	__vo uint32_t EMR;				/*Event mask register,	 						Address Offset:0x04*/
+	__vo uint32_t RTSR;				/*Rising trigger selection register, 			Address Offset:0x08*/
+	__vo uint32_t FTSR;				/*Falling trigger selection register, 			Address Offset:0x0C*/
+	__vo uint32_t SWIER;			/*Software interrupt event register, 			Address Offset:0x10*/
+	__vo uint32_t PR;				/*Pending register, 							Address Offset:0x14*/
+}EXTI_RegDef_t;
+
+/**
+ * Peripheral register definition structure of AFIO
+ */
+typedef struct
+{
+	__vo uint32_t EVCR;				/*Event control register,	 								Address Offset:0x00*/
+	__vo uint32_t MAPR;				/*AF remap and debug I/O configuration register1,	 		Address Offset:0x04*/
+	__vo uint32_t EXTICR[4];			/*External interrupt configuration register, 				Address Offset:0x08*/
+	uint32_t RESERVED;
+	__vo uint32_t MAPR2;			/*AF remap and debug I/O configuration register2, 			Address Offset:0x1C*/
+}AFIO_RegDef_t;
+
+
+
 /*********************************peripheral definitions (Peripheral Base Addresses typecasted to xxx_RegDef_t)*************************/
 #define GPIOA  			((GPIO_RegDef_t*)GPIOA_BASEADDR)
 #define GPIOB  			((GPIO_RegDef_t*)GPIOB_BASEADDR)
@@ -107,6 +163,14 @@ typedef struct
 #define GPIOG  			((GPIO_RegDef_t*)GPIOG_BASEADDR)
 
 #define RCC 			((RCC_RegDef_t*)RCC_BASEADDR)
+
+#define EXTI			((EXTI_RegDef_t*)EXT1_BASEADDR)
+#define AFIO			((AFIO_RegDef_t*)AFIO_BASEADDR)
+
+/*
+ *  Clock Enable Macros for AFIO peripherals
+ */
+#define AFIO_PCLK_EN()			( RCC->APB2ENR |= (1<<0) )
 
 /*
  *  Clock Enable Macros for GPIOx peripherals
@@ -144,7 +208,10 @@ typedef struct
 #define UART4_PCLK_EN()				( RCC->APB1ENR |= (1<<19) )
 #define UART5_PCLK_EN()				( RCC->APB1ENR |= (1<<20) )
 
-
+/*
+ *  Clock Disable Macros for AFIO peripherals
+ */
+#define AFIO_PCLK_DI()			( RCC->APB2ENR &= ~(1<<0) )
 /*
  *  Clock Disable Macros for GPIOx peripherals
  */
@@ -193,6 +260,29 @@ typedef struct
 #define GPIOE_REG_RESET()		do{ (RCC->APB2ENR |= (1<<6));  (RCC->APB2ENR &= ~(1<<6)); }while(0)
 #define GPIOF_REG_RESET()		do{ (RCC->APB2ENR |= (1<<7));  (RCC->APB2ENR &= ~(1<<7)); }while(0)
 #define GPIOG_REG_RESET()		do{ (RCC->APB2ENR |= (1<<8));  (RCC->APB2ENR &= ~(1<<8)); }while(0)
+
+
+/*
+ * Encoded GPIO port value used to map a GPIO port to the corresponding EXTI line
+ */
+#define GPIO_BASEADDR_TO_CODE(x)		((x==GPIOA) ? 0 : \
+										 (x==GPIOB) ? 1 : \
+										 (x==GPIOC) ? 2 : \
+										 (x==GPIOD) ? 3 : \
+										 (x==GPIOE) ? 4 : \
+										 (x==GPIOF) ? 5 : 6)
+
+/*
+ * Interrupt Request (IRQ) Number
+ */
+#define IRQ_NO_EXTI0				6
+#define IRQ_NO_EXTI1				7
+#define IRQ_NO_EXTI2				8
+#define IRQ_NO_EXTI3				9
+#define IRQ_NO_EXTI4				10
+#define IRQ_NO_EXTI9_5				23
+#define IRQ_NO_EXTI15_10			40
+
 
 /*
  * Generic Macros
--- drivers/Src/stm32f103xx_gpio_driver.c
+++ drivers/Src/stm32f103xx_gpio_driver.c
@@ -75,12 +75,66 @@ void GPIO_Init(GPIO_Handle_t *pGPIOHandle) {
 	//2. Configure the speed using Mode
 	//3. Configure the pupd settings
 	//4. Configure the output type
+	uint8_t pin = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber;
 
 	if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode >= GPIO_MODE_IT_FT) {
 		//the interrupt mode
+
+		if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_IT_FT) {
+			//1.Configure the FTSR
+			EXTI->FTSR |= (1 << pin);
+			//1.Clear corresponding RTSR
+			EXTI->RTSR &= ~(1 << pin);
+
+		} else if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_IT_RT) {
+			//1.Configure the RTSR
+			EXTI->RTSR |= (1 << pin);
+			//1.Clear corresponding FTSR
+			EXTI->FTSR &= ~(1 << pin);
+		} else if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_IT_RFT) {
+			//1.Configure both the RTSR and FTSR
+
+			EXTI->FTSR |= (1 << pin);
+			EXTI->RTSR |= (1 << pin);
+		}
+		//2. Configure the GPIO port selection in AFIO_EXTICR
+		/*
+		 * Configure GPIO port source for a particular EXTI line
+		 *
+		 * temp1 -> Selects EXTICR register index
+		 *          EXTICR[0] : EXTI0  - EXTI3
+		 *          EXTICR[1] : EXTI4  - EXTI7
+		 *          EXTICR[2] : EXTI8  - EXTI11
+		 *          EXTICR[3] : EXTI12 - EXTI15
+		 *
+		 * temp2 -> Calculates bit position inside selected EXTICR register
+		 *          Each EXTI line occupies 4 bits
+		 *
+		 * Clear existing port selection and configure new GPIO port
+		 * for the required EXTI line.
+		 */
+		uint8_t temp1 = pin / 4; 		 			// Select EXTICR register
+		uint8_t temp2 = (pin % 4) * 4;				// Calculate bit position
+
+		/*
+		 * portCode -> Encoded GPIO port value used to map
+		 *             a GPIO port to the corresponding EXTI line
+		 *
+		 * GPIOA = 0x0
+		 * GPIOB = 0x1
+		 * GPIOC = 0x2
+		 * GPIOD = 0x3
+		 */
+		uint8_t portcode = GPIO_BASEADDR_TO_CODE(pGPIOHandle->pGPIOx);
+
+		AFIO->EXTICR[temp1] &= ~(0xF << temp2); 	// Clear existing mapping
+		AFIO->EXTICR[temp1] |= (portcode << temp2); // Set new GPIO port mapping
+
+		//3. Enable the EXTI interrupt delivery using IMR
+		EXTI->IMR |= (1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+
 	} else {
 		//non interrupt mode
-		uint8_t pin = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber;
 		uint32_t position;
 		volatile uint32_t *configReg;
 		if (pin < 8) {
@@ -167,7 +221,7 @@ uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber) {
 
 	uint8_t value;
 
-	value = (uint8_t)(pGPIOx->IDR >> PinNumber) & 00000001 ;
+	value = (uint8_t) (pGPIOx->IDR >> PinNumber) & 00000001;
 
 	return value;
 
@@ -189,7 +243,7 @@ uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx) {
 
 	uint16_t value;
 
-	value = (uint16_t)(pGPIOx->IDR);
+	value = (uint16_t) (pGPIOx->IDR);
 
 	return value;
 }
@@ -209,18 +263,14 @@ uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx) {
 
  */
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber,
-		uint8_t Value)
-{
-		if(Value == GPIO_PIN_SET)
-		{
-			//Write 1 to the output data register at the bit field corresponding to the pin number
-			pGPIOx->ODR |= (1 << PinNumber);
-		}
-		else
-		{
-			//Write 0 to the output data register at the bit field corresponding to the pin number
-			pGPIOx->ODR &= ~(1 << PinNumber);
-		}
+		uint8_t Value) {
+	if (Value == GPIO_PIN_SET) {
+		//Write 1 to the output data register at the bit field corresponding to the pin number
+		pGPIOx->ODR |= (1 << PinNumber);
+	} else {
+		//Write 0 to the output data register at the bit field corresponding to the pin number
+		pGPIOx->ODR &= ~(1 << PinNumber);
+	}
 
 }
 
@@ -238,8 +288,7 @@ void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber,
  * @Note              - Set the 16 bit value to ODR register
 
  */
-void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value)
-{
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value) {
 	pGPIOx->ODR = Value;
 }
 
@@ -255,28 +304,91 @@ void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value)
  *
  * @Note              - Use XOR operation for toggling.
  * 						XOR with 1 → flips the bit
-						XOR with 0 → leaves it unchanged
+ XOR with 0 → leaves it unchanged
 
  */
-void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
-{
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber) {
 	pGPIOx->ODR ^= (1 << PinNumber);
 }
 /*********************************************************************
  * @fn      		  - SPI_IRQPriorityConfig
  *
- * @brief             -
+ * @brief             - Configuring the interrupt from processor side (Cortex M3 Generic User Guide )
  *
- * @param[in]         -
- * @param[in]         -
- * @param[in]         -
+ * @param[in]         - IRQ number: unique number assigned to each interrupt source inside the NVIC
+ * @param[in]         - EnorDi : Enable or Disable the interrupt
  *
- * @return            -
+ * @return            - void
  *
- * @Note              -
+ * @Note              - This function enables/disables the interrupt in NVIC and configures its priority.
 
  */
-void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi) {
+void GPIO_IRQConfig(uint8_t IRQNumber,uint8_t EnorDi) {
+	if (EnorDi == ENABLE) {
+		if (IRQNumber <= 31) {
+
+			*NVIC_ISER0 |= (1 << IRQNumber);
+			// program ISER0
+		} else if (IRQNumber >= 32 && IRQNumber < 63) {
+			// program ISER1
+			*NVIC_ISER1 |= (1 << (IRQNumber % 32));
+		} else if (IRQNumber >= 64 && IRQNumber < 96) {
+			// program ISER2
+			*NVIC_ISER2 |= (1 << (IRQNumber % 64));
+		}
+	} else {
+		if (IRQNumber <= 31) {
+			// program ICER0
+			*NVIC_ICER0 |= (1 << IRQNumber);
+		} else if (IRQNumber >= 32 && IRQNumber < 63) {
+			// program ICER1
+			*NVIC_ICER1 |= (1 << (IRQNumber % 32));
+		} else if (IRQNumber >= 64 && IRQNumber < 96) {
+			// program ICER2
+			*NVIC_ICER2 |= (1 << (IRQNumber % 64));
+		}
+	}
+
+}
+/*********************************************************************
+ * @fn      		  - GPIO_IRQInterruptConfig
+ *
+ * @brief             - Configures the priority of a given IRQ (Interrupt Request)
+ * 						using the NVIC Interrupt Priority Registers (IPR).
+ *
+ * @param[in]         - IRQNumber    : IRQ number whose priority must be configured
+ * @param[in]         - IRQPriority  : Priority value to be assigned
+ *
+ * @return            - void
+ * Working:
+ *  1. Each NVIC IPR register is 32 bits wide.
+ *  2. One IPR register contains priority fields for 4 IRQs.
+ *  3. IRQNumber/4 determines which IPR register to use.
+ *  4. IRQNumber%4 determines the section inside that register.
+ *  5. The priority value is shifted to the correct byte position
+ *     and written into the corresponding priority field.
+ *
+ * Formula:
+ *  IPR register index  = IRQNumber / 4
+ *  Section within IPR  = IRQNumber % 4
+ *
+ * Example:
+ *  IRQNumber = 6
+ *      -> Stored in IPR1
+ *      -> Section = 2
+ *
+ * @Note              -  NVIC priority registers are byte-addressable.
+ *  					Lower priority value means higher interrupt priority.
+
+ */
+void GPIO_IRQInterruptConfig(uint8_t IRQNumber,uint8_t IRQPriority) {
+
+	//1.Find out the IPR register
+	uint8_t iprx = (IRQNumber/4)*4;
+	uint8_t iprx_section = IRQNumber%4;
+	uint8_t shift_amount = (8 * iprx_section) + (8- NO_PR_BITS_IMPLEMENTED);
+	*(NVIC_PR_BASEADDR + iprx) |=(IRQPriority << shift_amount);
+
 }
 /*********************************************************************
  * @fn      		  - GPIO_IRQHandling
@@ -293,5 +405,11 @@ void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi) {
 
  */
 void GPIO_IRQHandling(uint8_t PinNumber) {
+
+	//clear the exti pr register corresponding to the pin number
+	if( EXTI->PR & (1<< PinNumber) ){
+		//clear
+		EXTI->PR = (1<<PinNumber);
+	}
 }
 
